acf2b8ca663e6fcabf2a21870204cbdc2221e9ee
Máximo Jofré <maximoalexandro464@gmail.com>
2025-11-22 19:00:09 -0300

    formato para la documentación

--- backend/api3/database.py
+++ backend/api3/database.py
@@ -2,8 +2,8 @@ import asyncpg
 
 async def get_connection():
     return await asyncpg.connect(
-        user="postgres",
-        password="123",
+        user="Mi_Usuario_Postgres",
+        password="Mi_Constraseña_Postgres",
         database="monster_high",
         host="fastapi-db",
         port="5432"
